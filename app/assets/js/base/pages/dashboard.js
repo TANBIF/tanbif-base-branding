dd6ad805792b3162e39a82a0b424c1f7f5b97bbb
Gisacc54 <giftisacc54@gmail.com>
2020-05-15 13:40:59 +0300

    Dashboard

---
+++ app/assets/js/base/pages/dashboard.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $.ajax({
+    url: "https://records-ws.tanbif.ditnet.ac.tz/occurrences?jsoncallback=?",
+    success: function(result){
+  $('#occurrence').html(result['totalRecords']);
+  }});
+  $.ajax({
+    url: "https://collections.tanbif.ditnet.ac.tz/ws/dataResource/count", 
+    success: function(result){
+  $('#datasets').html(result['total']);
+  }});
+  $.ajax({url: "https://collections.tanbif.ditnet.ac.tz/ws/institution/count",crossDomain: true, success: function(result){
+  $('#institutions').html(result['total']);
+  }});
+  $.ajax({url: "https://collections.tanbif.ditnet.ac.tz/ws/collection/count",crossDomain: true, success: function(result){
+  $('#collections').html(result['total']);
+  }});
+});
+
